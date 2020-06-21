34bbdaccc1f0b420249854c1fbf95e8e7c1a7177
lrf141 <lucky.rabbitfoot141@gmail.com>
2020-06-22 00:36:54 +0900

    remove exec func

--- httpserver/http.go
+++ httpserver/http.go
@@ -1,29 +1,2 @@
 package main
 
-import (
-	"fmt"
-	"io"
-	"log"
-	"net"
-)
-
-func exec(conn net.Conn) {
-	defer conn.Close()
-	var buf = make([]byte, 1024)
-	n, err := conn.Read(buf)
-	if err != nil {
-		if err == io.EOF {
-			return
-		} else {
-			log.Fatal(err)
-		}
-	}
-
-	fmt.Printf("Client> %s\n", buf)
-	fmt.Printf("Clinet> Size %d\n", n)
-
-	n, err = conn.Write(buf[:n])
-	if err != nil {
-		log.Fatal(err)
-	}
-}
